3176580965b0edd1911e500fe208905dc03d66bd
vince <vinceiansuralta3@gmail.com>
2025-08-13 20:56:59 +0800

    Added Tab

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,12 +1,58 @@
 import CardSection from "../sections/cardSection";
 import "../style/Body.css";
+import { useState } from "react";
 
-function Body(){
-    return(
-        <div id="cardContainer">
-            <CardSection/>
-        </div>
-    );
+function Body() {
+  // store which span is active
+  const [activeTab, setActiveTab] = useState<string>("Assignments");
+
+  // click handler
+  const handleClick = (tabName: string) => {
+    setActiveTab(tabName); // change active tab
+  };
+
+  // helper to check if tab is active
+  const getTabStyle = (tabName: string) => {
+    return tabName === activeTab
+      ? {
+            position:"relative",
+            bottom: "clamp(5%,1vw,50%)",
+            height: "35px",
+        }
+      : {};
+  };
+
+  return (
+    <>
+      <div id="BodyContainer"></div>
+      <div id="selection">
+        <span
+          id="Assignments"
+          style={getTabStyle("Assignments")}
+          onClick={() => handleClick("Assignments")}
+        >
+          ASSIGNMENTS
+        </span>
+        <span
+          id="ongoing"
+          style={getTabStyle("Ongoing")}
+          onClick={() => handleClick("Ongoing")}
+        >
+          ONGOING
+        </span>
+        <span
+          id="finished"
+          style={getTabStyle("Finished")}
+          onClick={() => handleClick("Finished")}
+        >
+          FINISHED
+        </span>
+      </div>
+      <div id="cardContainer">
+        <CardSection />
+      </div>
+    </>
+  );
 }
 
 export default Body;
